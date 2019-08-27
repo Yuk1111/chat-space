fc566ecdb02c3882130116788592f135ca88d261
Yuk1111 <gin_yuk1010@yahoo.co.jp>
2019-08-27 16:06:56 +0900

    1

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
 
-  root to: 'messages#index'  
- 
+  
+  devise_for :users
+  root 'groups#index'
+  # root to: 'groups#index'  
+
+    resources :users, only: [:index, :edit, :update]
+    resources :groups, only: [:new, :create, :edit, :update] do
+      resources :messages, only: [:index, :create]
+    end
+  end
   # messages_controllerのindexアクションがルートパス
   #For details on the DSL available within this file, 
   #see http://guides.rubyonrails.org/routing.html
-end
+
