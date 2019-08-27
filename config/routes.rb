c87d4da86c6a39a7a404563e1f34e5767e960800
Yuk1111 <gin_yuk1010@yahoo.co.jp>
2019-08-27 17:29:14 +0900

    1

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,8 @@ Rails.application.routes.draw do
   
   devise_for :users
   root 'groups#index'
-  # root to: 'groups#index'  
 
-    resources :users, only: [:index, :edit, :update]
+    resources :user, only: [:index, :edit, :update]
     resources :groups, only: [:new, :create, :edit, :update] do
       resources :messages, only: [:index, :create]
     end
@@ -14,3 +13,13 @@ Rails.application.routes.draw do
   #For details on the DSL available within this file, 
   #see http://guides.rubyonrails.org/routing.html
 
+
+
+  # Rails.application.routes.draw do
+  #   devise_for :users
+  #   root 'groups#index'
+  #   resource :user, only: [:edit, :update]
+  #   resources :groups, only: [:new, :create, :edit, :update] do
+  #     resources :messages, only: [:index, :create]
+  #   end
+  # end
