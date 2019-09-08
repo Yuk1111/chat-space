0cd78efc2f52ac3a61dbd8e0d1f8ccac1ddfc543
Yuk1111 <gin_yuk1010@yahoo.co.jp>
2019-09-08 14:33:57 +0900

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -11,8 +11,9 @@ module ChatSpace
       g.javascripts false
       g.helper false
       g.test_framework false
+      g.i18n.default_locale = :ja
+      g.time_zone = 'Tokyo'
+
     end
-      config.i18n.default_locale = :ja
-      config.time_zone = 'Tokyo'
   end
 end
