dcb8064e33967f56f6a3e4a31190407c875cb820
Yuk1111 <gin_yuk1010@yahoo.co.jp>
2019-09-08 14:36:22 +0900

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -6,14 +6,14 @@ Bundler.require(*Rails.groups)
 
 module ChatSpace
   class Application < Rails::Application
-    config.generators do |g|
-      g.stylesheets false
-      g.javascripts false
-      g.helper false
-      g.test_framework false
-      g.i18n.default_locale = :ja
-      g.time_zone = 'Tokyo'
+    # config.generators do |g|
+    #   g.stylesheets false
+    #   g.javascripts false
+    #   g.helper false
+    #   g.test_framework false
+      config.i18n.default_locale = :ja
+      config.time_zone = 'Tokyo'
 
-    end
+    # end
   end
 end
